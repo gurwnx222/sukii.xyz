9c02ebe4c54e2b9afee9293b1cdc02fade68ec39
Shivam Shori <shivamshori08@gmail.com>
2025-08-01 16:41:57 +0530

    added testing and events pages under maintenance

---
+++ app/events/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Maintenance from "@/features/maintenance/maintenance";
+export default function Events() {
+  return (
+    <div>
+      <Maintenance />
+    </div>
+  );
+}
---
+++ app/testing/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Maintenance from "@/features/maintenance/maintenance";
+export default function Testing() {
+  return (
+    <div>
+      <Maintenance />
+    </div>
+  );
+}
