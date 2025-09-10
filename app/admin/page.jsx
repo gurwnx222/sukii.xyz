d32bf5a44e1ed36e6494cbcc94cf6cb8deed56b1
Gurwinder Singh <gurwinx369@gmail.com>
2025-09-10 06:12:20 +0530

    removed admin page

--- app/admin/page.jsx
+++
@@ -1,6 +0,0 @@
-/*import React from "react";
-//import EventAdminDashboard from "@/features/events/admin/EventAdminDashboard";
-export default function page() {
-  return <EventAdminDashboard />;
-}
-*/
